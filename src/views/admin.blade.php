e90f2d07ddba85d454821c411e155267879d5677
chris15001900 <krzysztofbaczkiewicz@gmail.com>
2014-10-30 14:58:28 +0100

    Added settings button

--- src/views/admin.blade.php
+++ src/views/admin.blade.php
@@ -6,6 +6,10 @@
 
 	<div class=""> 
 
+		<a href="{{ url('admin/gallery/settings') }}" class="btn btn-default btn-lg pull-right" style="margin-left: 10px;">
+			<i class="glyphicon glyphicon-cog"></i>
+		</a>
+
 		<a href="{{ url('admin/gallery/create') }}" style="margin-left: 10px" class="btn btn-lg btn-success pull-right">
 			Create new album
 		</a>
